a4fc33949ffbadd8998cd420546bd6914fad858c
chalinor.baliuag <chalinor.baliuag@B204-TUTOR.soit.local>
2017-07-28 14:29:44 +1200

    Changed the package name from login to login1. Added Frame2.

---
+++ Assignment1/src/login1/Frame2.java
@@ -0,0 +1,51 @@
+package login1;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+import java.awt.Color;
+
+public class Frame2 extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Frame2 frame = new Frame2();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Frame2() {
+		setTitle("Frame2");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 339, 225);
+		contentPane = new JPanel();
+		contentPane.setBackground(Color.PINK);
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblWelcomeToFrame = new JLabel("Welcome to Frame 2");
+		lblWelcomeToFrame.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblWelcomeToFrame.setBounds(80, 65, 200, 33);
+		contentPane.add(lblWelcomeToFrame);
+	}
+}
--- Assignment1/src/login1/Login.java
+++ Assignment1/src/login1/Login.java
@@ -1,20 +1,24 @@
-package login;
+package login1;
 
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
 import java.awt.Font;
 import javax.swing.JTextField;
 import javax.swing.JPasswordField;
 import javax.swing.JButton;
 import java.awt.Color;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Login {
 
 	private JFrame frmLogin;
-	private JTextField textField;
-	private JPasswordField passwordField;
+	private JTextField txtUsername;
+	private JPasswordField txtPassword;
 
 	/**
 	 * Launch the application.
@@ -55,21 +59,35 @@ public class Login {
 		lblUsername.setBounds(10, 25, 84, 22);
 		frmLogin.getContentPane().add(lblUsername);
 		
-		textField = new JTextField();
-		textField.setBounds(90, 27, 147, 20);
-		frmLogin.getContentPane().add(textField);
-		textField.setColumns(10);
+		txtUsername = new JTextField();
+		txtUsername.setBounds(90, 27, 147, 20);
+		frmLogin.getContentPane().add(txtUsername);
+		txtUsername.setColumns(10);
 		
 		JLabel lblPassword = new JLabel("Password");
 		lblPassword.setFont(new Font("Tahoma", Font.PLAIN, 13));
 		lblPassword.setBounds(10, 70, 66, 22);
 		frmLogin.getContentPane().add(lblPassword);
 		
-		passwordField = new JPasswordField();
-		passwordField.setBounds(90, 72, 147, 20);
-		frmLogin.getContentPane().add(passwordField);
+		txtPassword = new JPasswordField();
+		txtPassword.setBounds(90, 72, 147, 20);
+		frmLogin.getContentPane().add(txtPassword);
 		
 		JButton btnLogin = new JButton("Login");
+		btnLogin.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				if((txtUsername.getText().equals("user")) && (txtPassword.getText().equals("1234"))) {
+				JOptionPane.showMessageDialog(null, "Correct username and password.");
+				frmLogin.dispose();
+				
+				//access the other frame
+				Frame2 obj = new Frame2();
+				obj.setVisible(true);
+				
+				}		
+				
+			}
+		});
 		btnLogin.setBounds(90, 124, 89, 23);
 		frmLogin.getContentPane().add(btnLogin);
 	}
